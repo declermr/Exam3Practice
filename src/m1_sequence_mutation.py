854c7de3df67a7c60e194f986147988df79c4fbb
declermr <declermr@rose-hulman.edu>
2018-01-25 01:07:47 -0500

    committed m2 (and m1)

--- src/m1_sequence_mutation.py
+++ src/m1_sequence_mutation.py
@@ -5,8 +5,8 @@ This problem provides practice at:
   ***  LOOPS WITHIN LOOPS, SEQUENCES and MUTATION  ***
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Matthew De Clerck.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
@@ -34,6 +34,24 @@ def run_test_zero_changer():
     # TODO: 2. Write at least 2 additional tests for the  zero_changer   function.
     #   Try do do some unexpected things like empty lists or an empty tuple.
     # ------------------------------------------------------------------
+
+    # Test 2:
+    test2 = ([4, 9, 6, 2, 2], [0, 5, 0, 1, 6, 7], [3, 0, 9, 6], [8, 0, 0, 3])
+    expected2 = ([4, 9, 6, 2, 2], [1, 5, 2, 1, 6, 7], [3, 3, 9, 6], [8, 4, 5, 3])
+    zero_changer(test2)
+    print()
+    print('Test 2:')
+    print('  Expected:', expected2)
+    print('  Actual:  ', test2)
+
+    # Test 3:
+    test3 = ([1, 1, 0], [9, 0, 7, 8, 2, 0], [8, 0, 8])
+    expected3 = ([1, 1, 1], [9, 2, 7, 8, 2, 3], [8, 4, 8])
+    zero_changer(test3)
+    print()
+    print('Test 3:')
+    print('  Expected:', expected3)
+    print('  Actual:  ', test3)
 
 
 def zero_changer(tuple_of_lists):
@@ -69,6 +87,16 @@ def zero_changer(tuple_of_lists):
     #    DIFFICULTY:      7
     #    TIME ESTIMATE:  10 minutes.
     # ------------------------------------------------------------------
+
+    count = 1
+    for k in range(len(tuple_of_lists)):
+        subset = tuple_of_lists[k]
+        for j in range(len(subset)):
+            if subset[j] == 0:
+                subset[j] = count
+                count += 1
+
+    return tuple_of_lists
 
 
 # ----------------------------------------------------------------------
